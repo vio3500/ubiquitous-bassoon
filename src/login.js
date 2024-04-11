41b30e60e0297c0de3704437c4a6a112405e3ad9
vio3500 <vio3500@github>
2024-04-11 16:18:52 +0800

    Updated: login page and CSS file

--- src/login.js
+++ src/login.js
@@ -1,3 +1,4 @@
+import './login.css';
 import React, { useState } from 'react';
 import { Typography } from '@douyinfe/semi-ui';
 import { Input } from '@douyinfe/semi-ui';
@@ -6,38 +7,53 @@ import { Button } from '@douyinfe/semi-ui';
 function Login() {
     const { Title } = Typography;
     const { Text } = Typography;
+    const [accountID, setAccountID] = useState(undefined)
+    const [password, setPassword] = useState(undefined)
     const [loginResponse, setLoginResponse] = useState(undefined)
-    const [value, setValue] = useState(undefined)
+    const handleLogin = () => {
+        const generateRandomNumber = () => {
+            return Math.random();
+        }
+        const login = new Promise((resolve, reject) => {
+            if (generateRandomNumber() < 0.5) {
+                resolve("Login OK")
+            } else {
+                reject("Error: Access Denied. ErrorCode=0000")
+            }
+        })
+        login
+            .then((response) => {
+                setLoginResponse(response);
+                console.log(response)
+            })
+            .catch((error) => {
+                setLoginResponse(error);
+                console.log(error)
+            });
+    }
     return (
         <>
             <Title>登录</Title>
-            <Input onChange={ChangeValue=>{
-                setValue(ChangeValue);
-                console.log(ChangeValue);}
-            } placeholder='账号'></Input>
-            <Input mode="password" placeholder='密码'></Input>
-            <Button size='large' onClick={() => {
-                const generateRandomNumber = () => {
-                    return Math.random();
-                }
-                const login = new Promise((resolve, reject) => {
-                    if (generateRandomNumber() < 0.5) {
-                        resolve("Login OK")
-                    } else {
-                        reject("Error: Access Denied. ErrorCode=0000")
+            <Input onChange=
+                {
+                    AccountID=>
+                    {
+                        setAccountID(AccountID);
+                        console.log(AccountID);
                     }
-                })
-                login
-                    .then((response) => {
-                        setLoginResponse(response);
-                        console.log(response)
-                    })
-                    .catch((error) => {
-                        setLoginResponse(error);
-                        console.log(error)
-                    });
-            }}>登录</Button>
-            <Text>没有账号？<Text link={{ href: 'https://baidu.com/' }}>注册</Text></Text>
+                }
+                   placeholder='账号'></Input>
+            <Input mode="password" placeholder='密码' onChange=
+                {
+                    Password=>
+                    {
+                        setPassword(password);
+                        console.log(Password);
+                    }
+                }
+            ></Input>
+            <Button size='large' theme='solid' type='secondary' onClick={handleLogin}>登录</Button>
+            <Text>没有账号？<Text link={{ href: '' }}>注册</Text></Text>
         </>
     )
 }
