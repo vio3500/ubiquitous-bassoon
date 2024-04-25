90466e9fdb6c0c7e7b42b38f63dea9e88313afba
vio3500 <vio3500@github>
2024-04-25 16:14:03 +0800

    update

--- src/login.js
+++ src/login.js
@@ -1,19 +1,17 @@
 import './login.css';
 import React, {useEffect, useState} from 'react';
-import { Typography } from '@douyinfe/semi-ui';
-import { Input } from '@douyinfe/semi-ui';
-import { Button } from '@douyinfe/semi-ui';
+import {Button, Input, Typography} from '@douyinfe/semi-ui';
 
 function Login() {
-    const { Title } = Typography;
-    const { Text } = Typography;
+    const {Title} = Typography;
+    const {Text} = Typography;
     const [username, setUsername] = useState(undefined)
     const [password, setPassword] = useState(undefined)
     const [loginResponse, setLoginResponse] = useState(undefined)
     const [loginDeny, setLoginDeny] = useState(true)
     useEffect((() => {
-        if (username !== undefined && username !== ''){
-            if (password !== undefined && password !== ''){
+        if (username !== undefined && username !== '') {
+            if (password !== undefined && password !== '') {
                 setLoginDeny(false)
             } else {
                 setLoginDeny(true)
@@ -21,7 +19,7 @@ function Login() {
         } else {
             setLoginDeny(true)
         }
-    }),[username, password])
+    }), [username, password])
     const handleLogin = () => {
         const login = new Promise((resolve, reject) => {
             if (Math.random() <= 0.5) {
@@ -42,25 +40,25 @@ function Login() {
     }
     return (
         <>
-            <Title>登录</Title>
-            <Input onChange=
-                {
-                    username=>
+            <div id={'login'}>
+                <Title>登录</Title>
+                <Input onChange=
+                        {
+                            username => {
+                                setUsername(username)
+                            }
+                        }
+                    placeholder='账号'></Input>
+                <Input mode="password" placeholder='密码' onChange=
                     {
-                        setUsername(username)
+                        password => {
+                            setPassword(password)
+                        }
                     }
-                }
-                   placeholder='账号'></Input>
-            <Input mode="password" placeholder='密码' onChange=
-                {
-                    password=>
-                    {
-                        setPassword(password)
-                    }
-                }
-            ></Input>
-            <Button disabled={loginDeny} size='large' theme='solid' type='secondary' onClick={handleLogin}>登录</Button>
-            <Text>没有账号？<Text link={{ href: '' }}>注册</Text></Text>
+                ></Input>
+                <Button disabled={loginDeny} size='large' theme='solid' type='secondary' onClick={handleLogin}>登录</Button>
+                <Text>没有账号？<Text link={{href: ''}}>注册</Text></Text>
+            </div>
         </>
     )
 }
